ef461729ec686dd6584a8a52093cf047409da46a
Travis Offtermatt <travis.offtermatt@gmail.com>
2017-09-18 11:35:03 -0600

    Adding messages regarding harmony postfixes at startup

--- Source/Main.cs
+++ Source/Main.cs
@@ -21,7 +21,10 @@ namespace ChangeDresser
 
             WidgetUtil.Initialize();
             
+            Log.Message("ChangeDresser: Adding Harmony Postfix to Pawn.GetGizmos");
+            Log.Message("ChangeDresser: Adding Harmony Postfix to Pawn_ApparelTracker.Notify_ApparelAdded");
             Log.Message("ChangeDresser: Adding Harmony Postfix to Pawn_DraftController.Drafted { set }");
+            Log.Message("ChangeDresser: Adding Harmony Postfix to Pawn_DraftController.GetGizmos");
             Log.Message("ChangeDresser: Adding Harmony Postfix to JobGiver_OptimizeApparel.TryGiveJob(Pawn)");
         }
 
